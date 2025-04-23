082529f71590270e3202285a247c6144bac458a5
TheSilentWave <mail@thesilentwave.de>
2025-04-23 13:06:31 +0200

    testing workflow 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pysilentwave",
-    version="0.1.1",
+    version="0.1.2",  # Updated from 0.1.1
     description="API client for TheSilentWave devices",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
